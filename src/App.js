7de8dae4aceee7d94de23552c322147d64d4b27c
april067 <ik.kiev.ua@gmail.com>
2024-10-22 17:32:34 +0300

    ready to get control to redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
+import { connect } from 'react-redux';
+
 // import logo from './logo.svg';
 import './App.css';
 import DisplayMessages from './DisplayMessages';
+import { mapStateToProps, mapDispatchToProps } from './store';
 
 function App(props) {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<DisplayMessages messages={props.messages} submitNewMessage={props.submitNewMessage} />
-
-				{/* <img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a> */}
+				<ConnectedComponent />
 			</header>
 		</div>
 	);
 }
+
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(DisplayMessages);
 
 export default App;
